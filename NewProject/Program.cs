aeba299617422d521bf840533d1a5168445ebfcb
devel <samuelarango06@gmail.com>
2025-03-03 17:58:31 -0500

    Init Project

---
+++ NewProject/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace NewProject{
+    class Program{
+        static int Elevar(){
+            Console.WriteLine($"Por favor agregar el valor {value}");
+            int value = Console.ReadLine();
+            for (int i = 0; i < length; i++)
+            {
+                
+            }
+            return value;
+
+        }
+        static void Main(){
+            int? num = null; 
+            Console.WriteLine("Hello Word!");
+            Console.WriteLine($"Valores {num}");
+        }
+    }
+
+    public class Pesona
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateOnly Birday { get; set; }
+    }
+
+}
